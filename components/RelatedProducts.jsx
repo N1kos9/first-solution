7b322f381080e498500390dbfc7584bbac718c1f
N1kos9 <onkl3p@gmail.com>
2024-01-31 20:12:23 +0000

    ortograohy

--- components/RelatedProducts.jsx
+++ components/RelatedProducts.jsx
@@ -20,7 +20,7 @@ const RelatedProducts = ({ products }) => {
 
   return (
     <div className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">
-      <div className="text-2xl font-bold mb-5">You might also like</div>
+      <div className="text-2xl font-bold mb-5">You might also like:</div>
       <Carousel
         responsive={responsive}
         containerClass="-mx-[10px]"
